70d8936c1e8a10b54c58c1f0c4d3e1c28491c1ae
Samuel López <salopfe@gmail.com>
2026-02-18 01:00:40 +0100

    Nueva versión inglés

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Función para hacer que la navegación entre secciones sea fluida
+// Smooth scroll for navigation
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
         const targetId = this.getAttribute('href').substring(1);
         const targetElement = document.getElementById(targetId);
 
-        // Desplazarse suavemente a la sección correspondiente
+        // Scroll smoothly to the section
         window.scrollTo({
-            top: targetElement.offsetTop - 50, // Ajustamos el desplazamiento para que la sección no quede tapada por el menú
+            top: targetElement.offsetTop - 50, // Adjust for the sticky nav bar
             behavior: 'smooth'
         });
     });
